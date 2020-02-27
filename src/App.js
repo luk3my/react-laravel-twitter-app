165a4bd661a9cefe1e392ba2227ff71908a0bec1
luk3my <luk3myoung@gmail.com>
2020-02-27 16:47:30 +1000

    render with loading

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import Loading from './loading';
+import "./App.css";
 
 class App extends Component {
-  
-constructor(props) {
-  super(props)
-  
-  // state
-  this.state = {
-    users: []
+  constructor(props) {
+    super(props);
+
+    // state
+    this.state = {
+      users: [],
+      loading: false
+    };
+  }
+
+  getUsers() {
+    this.setState({
+      loading: true
+    });
+    Axios(`https://api.randomuser.me/?nat=US&results=5`).then(response =>
+      this.setState({
+        users: response.data.results,
+        loading: false
+      })
+    );
+  }
+
+  componentWillMount() {
+    this.getUsers();
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {!this.state.loading ? (
+          this.state.users.map(user => (
+            <div key={user.id.value}>
+              <h3>{user.name.first}</h3>
+              <p>{user.email}</p>
+              <hr />
+            </div>
+          ))
+        ) : (
+          <Loading />
+        )}
+      </div>
+    );
   }
 }
-
-componentWillMount() {
-
-  Axios(`https://api.randomuser.me/?nat=US&results=5`)
-  .then(response => this.setState({
-    users: response.data.results
-   })
-  );
- }
-
-  render(){
-
-    return <div className='app'>
-      {this.state.users.map(user => <div key={user.id.value}>{user.cell}</div>)}
-      </div>;
-  }
-};
 
 export default App;
---
+++ src/loading.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const loading = () => <h2>Loading</h2>;
+
+export default loading;
